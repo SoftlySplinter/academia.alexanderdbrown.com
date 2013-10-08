1baf933801010f97d74a71c2b32e6001f4e7f20a
Alex Brown <alex@alexanderdbrown.com>
2013-10-08 15:54:54 +0100

    Pull from http://users.aber.ac.uk/adb9/

---
+++ talks/index.php
@@ -0,0 +1,46 @@
+<?php require 'talks.php'; ?>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Alexander D Brown - Talks</title>
+    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="/default.css" rel="stylesheet" type="text/css">
+    <link href="/desktop.css" rel="stylesheet" type="text/css">
+    <link href="/talks/talk.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="header">
+      <h1>Alexander D Brown</h1>
+    </div>
+    <div id="navigation">
+      <ul id="navbar">
+        <li><a href="/">Home</a></li>
+        <li><a href="/mobile">Mobile</a></li>
+        <li class="selected"><a href="/talks">Talks</a></li>
+      </ul>
+    </div>
+    <div id="about">
+      <h2>About</h2>
+      <p>
+        These are links to some of my previous or upcoming talks, ordered by 
+        date.
+      </p>
+    </div>
+    <div id="content">
+      <h2>Talks and Presentations</h2>
+      <ul class="no-list">
+        <?php 
+          foreach(get_talks() as $talk) {
+            $html = '<li class="talk-name"><a href="%s">%s</a>
+                     <li class="talk-event">%s</li>
+                     <li class="talk-date">%s</li>';
+            $date = date('l j<\s\up>S</\s\up> F, Y', strtotime($talk->t_date));
+            echo sprintf($html, $talk->t_href, $talk->t_name, $talk->t_event, $date);
+          }
+        ?>
+      </ul>
+    </div>
+    <div id="footer">
+    </div>
+  </body>
+</html>
---
+++ talks/talks.php
@@ -0,0 +1,35 @@
+<?php
+  class Talk {
+    public $t_file;
+    public $t_name;
+    public $t_event;
+    public $t_date;
+
+    function __construct($talk) {
+      $temp = explode("\t", rtrim($talk));
+      $this->t_file = $temp[0];
+      $this->t_name = $temp[1];
+      $this->t_event = $temp[2];
+      $this->t_date = $temp[3];
+    }
+
+    function cmp($a, $b) {
+      return strtotime($a->t_date ) - strtotime($b->t_date);
+    }
+  }
+
+  $talk_file = '/talks/talks';
+
+  function get_talks() {
+    $talks = array();
+    $f = fopen('talks', 'r') or die('Could not open file: ' + $talk_file);
+    while(!feof($f)) {
+      $line = fgets($f);
+      if(!empty($line)) {
+        array_push($talks, new Talk($line));
+      }
+    }
+    usort($talks, array('Talk', 'cmp'));
+    return $talks;
+  }
+?>
