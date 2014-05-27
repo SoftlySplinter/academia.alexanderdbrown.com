e9587ec40145b2dc9bf89c7957c2e6e2d2f90f2d
Alexander Brown <alex@alexanderdbrown.com>
2014-05-27 18:59:30 +0100

    More updates, etc.

--- research/index.php
+++ research/index.php
@@ -14,65 +14,68 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/lib/essay.php');
     <link href="/desktop.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    <div id="header">
+    <header id="header">
       <h1>Alexander D Brown</h1>
-    </div>
-    <div id="navigation">
+    </header>
+    <nav id="navigation">
       <ul id="navbar">
         <li><a href="/">Home</a></li>
         <li class="selected"><a href="/research/">Research</a></li>
         <li><a href="/talks/">Talks</a></li>
         <li><a href="http://blog.alexanderdbrown.com">Blog</a></li>
       </ul>
-    </div>
-    <div id="about">
+    </nav>
+    <section id="about">
       <h2>About</h2>
       <p>
         My current and previous research, including
         <a href="#publications">publications</a> and 
         <a href="#essays">essays</a>.
       </p>
-    </div>
-    <div id="content">
-      <a name="publications"></a>
-      <h2>Publications</h2>
-      <ul class="no-list">
-      <?php
-        $publications = alexanderdbrown\academia\Publication::get('/data/publications');
-        if(empty($publications)) {
-          echo '<li class="empty">No Publications</li>';
-        }
-        foreach($publications as $pub) {
-          $html = '<li class="title"><a href="%s">%s</a></li>
-                   <li class="author">%s</li>
-                   <li class="conference">%s</li>
-                   <li class="date">%s</li>';
-          $date = date('F, Y', $pub->p_date);
-          echo sprintf($html, $pub->p_url, $pub->p_name, $pub->p_authors, $pub->p_journal, $date);
-        }
-      ?>
-      </ul>
-
-      <a name="essays"></a>
-      <h2>Essays</h2>
-      <ul class="no-list">
-      <?php 
-        $essays = alexanderdbrown\academia\Essay::get('/data/essays'); 
-        if(empty($essays)) {
-          echo '<li class="empty">No Essays</li>';
-        }
-        foreach($essays as $essay) {
-          $html = '<li class="title"><a href="%s">%s</a></li>
-                   <li class="author">%s</li>
-                   <li class="conference">%s</li>
-                   <li class="date">%s</li>';
-          $date = date('F, Y', $essay->date);
-          echo sprintf($html, $essay->url, $essay->name, $essay->author, $essay->audience, $date);
-        }
-      ?>
-      </ul>
-    </div>
-    <div id="footer">
-    </div>
+    </section>
+    <section id="content">
+      <article id="publications" name="publications">
+        <header>
+          <h2>Publications</h2>
+        </header>
+        <ul class="no-list">
+        <?php
+$publications = alexanderdbrown\academia\Publication::get('/data/publications');
+if(empty($publications)) {
+  echo '<li class="empty">No Publications</li>';
+}
+foreach($publications as $pub) {
+  $html = '<li class="title"><a href="%s">%s</a></li>
+           <li class="author">%s</li>
+           <li class="conference">%s</li>
+           <li class="date">%s</li>';
+  $date = date('F, Y', $pub->p_date);
+  echo sprintf($html, $pub->p_url, $pub->p_name, $pub->p_authors, $pub->p_journal, $date);
+}
+         ?>
+        </ul>
+      </article>
+      <article id="essays" name="essays"></a>
+        <h2>Essays</h2>
+        <ul class="no-list">
+        <?php 
+$essays = alexanderdbrown\academia\Essay::get('/data/essays'); 
+if(empty($essays)) {
+  echo '<li class="empty">No Essays</li>';
+}
+foreach($essays as $essay) {
+  $html = '<li class="title"><a href="%s">%s</a></li>
+           <li class="author">%s</li>
+           <li class="conference">%s</li>
+           <li class="date">%s</li>';
+  $date = date('F, Y', $essay->date);
+  echo sprintf($html, $essay->url, $essay->name, $essay->author, $essay->audience, $date);
+}
+         ?>
+        </ul>
+      </article>
+    </section>
+    <footer id="footer">
+    </footer>
   </body>
 </html>
--- talks/index.php
+++ talks/index.php
@@ -12,44 +12,46 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/lib/talks.php');
     <link href="/desktop.css" rel="stylesheet" type="text/css">
   </head>
   <body>
-    <div id="header">
+    <header id="header">
       <h1>Alexander D Brown</h1>
-    </div>
-    <div id="navigation">
+    </header>
+    <nav id="navigation">
       <ul id="navbar">
         <li><a href="/">Home</a></li>
         <li><a href="/research/">Research</a></li>
         <li class="selected"><a href="/talks/">Talks</a></li>
         <li><a href="http://blog.alexanderdbrown.com">Blog</a></li>
       </ul>
-    </div>
-    <div id="about">
+    </nav>
+    <section id="about">
       <h2>About</h2>
       <p>
         These are links to some of my previous or upcoming talks, ordered by 
         date.
       </p>
-    </div>
-    <div id="content">
-      <h2>Talks and Presentations</h2>
-      <ul class="no-list">
-        <?php 
-          $talks = alexanderdbrown\academia\Talk::get('/data/talks');
-          if(empty($talks)) {
-            echo '<li class="empty">No Talks</li>';
-          } else {
-            foreach($talks as $talk) {
-              $html = '<li class="title"><a href="%s">%s</a>
-                       <li class="conference">%s</li>
-                       <li class="date">%s</li>';
-              $date = date('l j<\s\up>S</\s\up> F, Y', $talk->t_date);
-              echo sprintf($html, $talk->t_file, $talk->t_name, $talk->t_event, $date);
-            }
-          }
-        ?>
-      </ul>
-    </div>
-    <div id="footer">
-    </div>
+    </section>
+    <section id="content">
+      <article id="talks">
+        <h2>Talks and Presentations</h2>
+        <ul class="no-list">
+          <?php 
+$talks = alexanderdbrown\academia\Talk::get('/data/talks');
+if(empty($talks)) {
+  echo '<li class="empty">No Talks</li>';
+} else {
+  foreach($talks as $talk) {
+    $html = '<li class="title"><a href="%s">%s</a>
+             <li class="conference">%s</li>
+             <li class="date">%s</li>';
+    $date = date('l j<\s\up>S</\s\up> F, Y', $talk->t_date);
+    echo sprintf($html, $talk->t_file, $talk->t_name, $talk->t_event, $date);
+  }
+}
+           ?>
+        </ul>
+      </article>
+    </section>
+    <footer id="footer">
+    </footer>
   </body>
 </html>
